d86838415b1c7a09d9751235f235fd2ab79baffc
Adhika <adhika.yumna@gmail.com>
2025-05-31 16:01:40 +0700

    Update Latwhile.py

--- Pertemuan4/Latwhile.py
+++ Pertemuan4/Latwhile.py
@@ -1,1 +1,14 @@
-
+while True:
+    userInput = int(input("Masukkan nilai: "))
+    tesGanjil = userInput % 2
+    
+    if(tesGanjil == 1):
+        print("Ini angka ganjil")
+    else: 
+        print ("ini angka genap")
+    
+    stopInput = input("Lanjut? (y/n)")
+    if(stopInput == "y"):
+        continue
+    elif(stopInput == "n"):
+        break
